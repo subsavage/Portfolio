b8a6a942094b82d1330435029f0ac038a68f9ce2
Harshit Srivastava <69175270+subsavage@users.noreply.github.com>
2024-10-28 18:44:19 +0530

    add parallax background

--- lib/pages/game_world.dart
+++ lib/pages/game_world.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:flame/components.dart';
+import 'package:flame/parallax.dart';
 import 'package:portfolio/pages/game.dart';
 import 'package:portfolio/sprites/player.dart';
 
@@ -8,8 +8,20 @@ class GameWorld extends World with HasGameRef<MainGame> {
   late final Player player;
 
   @override
-  FutureOr<void> onLoad() {
-    super.onLoad();
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    final parallax = await ParallaxComponent.load(
+      [ParallaxImageData('bg.png')],
+      baseVelocity: Vector2(10, 0),
+      velocityMultiplierDelta: Vector2(1.6, 1.0),
+    );
+
+    parallax.position = Vector2(0, 0);
+    parallax.size = gameRef.size;
+    parallax.anchor = Anchor.center;
+
+    add(parallax);
 
     player = Player();
     add(player);
